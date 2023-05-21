02c626723b18d75ef6491ed2fd359fca52d7407c
Martin Carlsson <martin@imus.dk>
2023-05-21 07:46:04 +0000

    Set Python version

--- .devcontainer/03_installPython.zsh
+++ .devcontainer/03_installPython.zsh
@@ -6,13 +6,14 @@ green="\033[0;32m"
 red="\033[0;31m"
 reset="\033[0m"
 
+PYTHON_VERSION=$(cat .python-version)
 
-echo "${blue}Installing Python 3.11 and pip ...${reset}"
-sudo apt-get install python3.11 python3-pip -y
+echo "${blue}Installing Python $PYTHON_VERSION and pip ...${reset}"
+sudo apt-get install python$PYTHON_VERSION python3-pip -y
 echo ""
 
-echo "${blue}Setting Python 3.11 as default ...${reset}"
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+echo "${blue}Setting Python $PYTHON_VERSION as default ...${reset}"
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$PYTHON_VERSION 1
 echo ""
 
 echo "${blue}Updating pip ...${reset}"
